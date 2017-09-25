dfcf9fa446d658f8e51ab50a501c3e2fae1d18b0
Dimitri Kopriwa <dimitri.kopriwa@kopaxgroup.com>
2017-09-25 07:45:16 +0700

    first commit

---
+++ dist/bootstrap-styled-utils.js
@@ -0,0 +1,166 @@
+(function (global, factory) {
+	typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+	typeof define === 'function' && define.amd ? define(['exports'], factory) :
+	(factory((global['bootstrap-styled-utils'] = {})));
+}(this, (function (exports) { 'use strict';
+
+function getTetherAttachments(placement) {
+  switch (placement) {
+    case 'top':
+    case 'top center':
+      return {
+        attachment: 'bottom center',
+        targetAttachment: 'top center'
+      };
+    case 'bottom':
+    case 'bottom center':
+      return {
+        attachment: 'top center',
+        targetAttachment: 'bottom center'
+      };
+    case 'left':
+    case 'left center':
+      return {
+        attachment: 'middle right',
+        targetAttachment: 'middle left'
+      };
+    case 'right':
+    case 'right center':
+      return {
+        attachment: 'middle left',
+        targetAttachment: 'middle right'
+      };
+    case 'top left':
+      return {
+        attachment: 'bottom left',
+        targetAttachment: 'top left'
+      };
+    case 'top right':
+      return {
+        attachment: 'bottom right',
+        targetAttachment: 'top right'
+      };
+    case 'bottom left':
+      return {
+        attachment: 'top left',
+        targetAttachment: 'bottom left'
+      };
+    case 'bottom right':
+      return {
+        attachment: 'top right',
+        targetAttachment: 'bottom right'
+      };
+    case 'right top':
+      return {
+        attachment: 'top left',
+        targetAttachment: 'top right'
+      };
+    case 'right bottom':
+      return {
+        attachment: 'bottom left',
+        targetAttachment: 'bottom right'
+      };
+    case 'left top':
+      return {
+        attachment: 'top right',
+        targetAttachment: 'top left'
+      };
+    case 'left bottom':
+      return {
+        attachment: 'bottom right',
+        targetAttachment: 'bottom left'
+      };
+    default:
+      return {
+        attachment: 'top center',
+        targetAttachment: 'bottom center'
+      };
+  }
+}
+var tetherAttachements = ['top', 'bottom', 'left', 'right', 'top left', 'top center', 'top right', 'right top', 'right middle', 'right bottom', 'bottom right', 'bottom center', 'bottom left', 'left top', 'left middle', 'left bottom'];
+function getScrollbarWidth() {
+  var scrollDiv = document.createElement('div');
+  scrollDiv.style.position = 'absolute';
+  scrollDiv.style.top = '-9999px';
+  scrollDiv.style.width = '50px';
+  scrollDiv.style.height = '50px';
+  scrollDiv.style.overflow = 'scroll';
+  document.body.appendChild(scrollDiv);
+  var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+  document.body.removeChild(scrollDiv);
+  return scrollbarWidth;
+}
+function setScrollbarWidth(padding) {
+  document.body.style.paddingRight = padding > 0 ? padding + 'px' : null;
+}
+function isBodyOverflowing() {
+  return document.body.clientWidth < window.innerWidth;
+}
+function getOriginalBodyPadding() {
+  return parseInt(window.getComputedStyle(document.body, null).getPropertyValue('padding-right') || 0, 10);
+}
+function conditionallyUpdateScrollbar() {
+  var scrollbarWidth = getScrollbarWidth();
+  var fixedContent = document.querySelectorAll('.navbar-fixed-top, .navbar-fixed-bottom, .is-fixed')[0];
+  var bodyPadding = fixedContent ? parseInt(fixedContent.style.paddingRight || 0, 10) : 0;
+  if (isBodyOverflowing()) {
+    setScrollbarWidth(bodyPadding + scrollbarWidth);
+  }
+}
+function toHashCode(str) {
+  var hash = 0;
+  if (str.length === 0) {
+    return hash;
+  }
+  for (var i = 0; i < str.length; i += 1) {
+    var char = str.charCodeAt(i);
+    hash = (hash << 5) - hash + char;
+    hash = hash & hash;
+  }
+  return hash;
+}
+
+function parseTransition(transitions) {
+  if (!transitions) {
+    return [];
+  }
+  var sample = transitions;
+  var RULE_DELIMITER = ',';
+  var PROPERTY_DELIMITER = ' ';
+  var MS_UNIT = 'ms';
+  var TMP_STR = 'TMP';
+  var DEFAULT_PROPERTY = 'all';
+  var DEFAULT_DURATION = 0;
+  var DEFAULT_TIMING_FUNCTION = 'ease';
+  var DEFAULT_DELAY = 0;
+  var BEZIER_REGEX = /cubic-bezier\([^\)]+\)/gi;
+  var cubicBezierList = transitions.match(BEZIER_REGEX);
+  if (cubicBezierList) {
+    sample = sample.replace(BEZIER_REGEX, TMP_STR);
+  }
+  var transitionList = sample.split(RULE_DELIMITER).map(function (rule) {
+    var properties = rule.trim().split(PROPERTY_DELIMITER);
+    return {
+      property: properties[0] || DEFAULT_PROPERTY,
+      duration: properties[1] && !(properties[1].indexOf(MS_UNIT) !== -1) ? parseFloat(properties[1]) * 1000 : parseFloat(properties[1]) || DEFAULT_DURATION,
+      timingFunction: properties[2] && properties[2] !== TMP_STR ? properties[2] : cubicBezierList ? cubicBezierList.shift() : DEFAULT_TIMING_FUNCTION,
+      delay: properties[3] && !(properties[3].indexOf(MS_UNIT) !== -1) ? parseFloat(properties[3]) * 1000 : parseFloat(properties[3]) || DEFAULT_DELAY
+    };
+  });
+  return transitionList;
+}
+
+exports.parseTransition = parseTransition;
+exports.getTetherAttachments = getTetherAttachments;
+exports.tetherAttachements = tetherAttachements;
+exports.getScrollbarWidth = getScrollbarWidth;
+exports.setScrollbarWidth = setScrollbarWidth;
+exports.isBodyOverflowing = isBodyOverflowing;
+exports.getOriginalBodyPadding = getOriginalBodyPadding;
+exports.conditionallyUpdateScrollbar = conditionallyUpdateScrollbar;
+exports.toHashCode = toHashCode;
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+})));
+//# sourceMappingURL=bootstrap-styled-utils.js.map
---
+++ src/index.js
@@ -0,0 +1,2 @@
+export * from './tools';
+export { default as parseTransition } from './parseTransition';
