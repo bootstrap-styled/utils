0ff563d850010c93e41e8f47509d4f452cbbde08
Dimitri Kopriwa <dka@dev-12.yeutech.vn>
2018-12-16 22:14:16 +0700

    feat(dependencies): babel7, latest rollup, latest jest...

--- src/unitUtils/index.js
+++ src/unitUtils/index.js
@@ -1,16 +1,17 @@
 class UnitUtils {
-  /**
+  constructor() {
+    /**
    * Unit
    * @type {{EM: string, REM: string, PX: string, PERCENT: string}}
    */
-  UNIT = {
-    EM: 'em',
-    REM: 'rem',
-    PX: 'px',
-    PERCENT: '%',
-  };
+    this.UNIT = {
+      EM: 'em',
+      REM: 'rem',
+      PX: 'px',
+      PERCENT: '%',
+    };
 
-  /**
+    /**
    * @public
    * @description Math operations accepting units value and return calculated result with unit.
    * @example
@@ -27,26 +28,26 @@ class UnitUtils {
    * const div = math.divide('15px' / 5);
    * // OUTPUT '3px';
    */
-  math = {
-    addition: function addition(a, b) {
-      const unit = this.detectUnit(a) || this.detectUnit(b);
-      return (this.rmUnit(a) + this.rmUnit(b)) + unit;
-    }.bind(this),
-    subtract: function subtract(a, b) {
-      const unit = this.detectUnit(a) || this.detectUnit(b);
-      return (this.rmUnit(a) - this.rmUnit(b)) + unit;
-    }.bind(this),
-    multiply: function multiply(a, b) {
-      const unit = this.detectUnit(a) || this.detectUnit(b);
-      return (this.rmUnit(a) * this.rmUnit(b)) + unit;
-    }.bind(this),
-    divide: function divide(a, b) {
-      const unit = this.detectUnit(a) || this.detectUnit(b);
-      return (this.rmUnit(a) / this.rmUnit(b)) + unit;
-    }.bind(this),
-  };
+    this.math = {
+      addition: function addition(a, b) {
+        const unit = this.detectUnit(a) || this.detectUnit(b);
+        return (this.rmUnit(a) + this.rmUnit(b)) + unit;
+      }.bind(this),
+      subtract: function subtract(a, b) {
+        const unit = this.detectUnit(a) || this.detectUnit(b);
+        return (this.rmUnit(a) - this.rmUnit(b)) + unit;
+      }.bind(this),
+      multiply: function multiply(a, b) {
+        const unit = this.detectUnit(a) || this.detectUnit(b);
+        return (this.rmUnit(a) * this.rmUnit(b)) + unit;
+      }.bind(this),
+      divide: function divide(a, b) {
+        const unit = this.detectUnit(a) || this.detectUnit(b);
+        return (this.rmUnit(a) / this.rmUnit(b)) + unit;
+      }.bind(this),
+    };
 
-  /**
+    /**
    * @public
    * @description Return the unit from a string by priority : px/rem/em/percent
    * @param {String} value
@@ -61,26 +62,26 @@ class UnitUtils {
    *
    * // OUTPUT 'rem'
    */
-  detectUnit = (value) => {
-    let ext;
-    const valueStr = value.toString();
-    if (valueStr.match(this.UNIT.PX)) {
-      ext = this.UNIT.PX;
-    } else if (valueStr.match(this.UNIT.REM)) {
-      ext = this.UNIT.REM;
-    } else if (valueStr.match(this.UNIT.EM)) {
-      ext = this.UNIT.EM;
-    } else if (valueStr.match(this.UNIT.PERCENT)) {
-      ext = this.UNIT.PERCENT;
-    } else if (!isNaN(value)) { // eslint-disable-line no-restricted-globals
-      return null;
-    } else {
-      throw new Error(`detectUnit can't find unit for ${value}`);
-    }
-    return ext;
-  }
+    this.detectUnit = (value) => {
+      let ext;
+      const valueStr = value.toString();
+      if (valueStr.match(this.UNIT.PX)) {
+        ext = this.UNIT.PX;
+      } else if (valueStr.match(this.UNIT.REM)) {
+        ext = this.UNIT.REM;
+      } else if (valueStr.match(this.UNIT.EM)) {
+        ext = this.UNIT.EM;
+      } else if (valueStr.match(this.UNIT.PERCENT)) {
+        ext = this.UNIT.PERCENT;
+      } else if (!isNaN(value)) { // eslint-disable-line no-restricted-globals
+        return null;
+      } else {
+        throw new Error(`detectUnit can't find unit for ${value}`);
+      }
+      return ext;
+    };
 
-  /**
+    /**
    * @public
    * @description Convert a value string to float. If unit is undefined, it will try to guess it's value using {detectUnit}
    * @param {String} value
@@ -95,14 +96,14 @@ class UnitUtils {
    *
    * // OUTPUT '.5rem'
    */
-  rmUnit = (value, unit) => {
-    const valueStr = value.toString();
-    const ext = unit || this.detectUnit(valueStr);
-    const number = valueStr.replace(ext, '');
-    return parseFloat(number);
-  }
+    this.rmUnit = (value, unit) => {
+      const valueStr = value.toString();
+      const ext = unit || this.detectUnit(valueStr);
+      const number = valueStr.replace(ext, '');
+      return parseFloat(number);
+    };
 
-  /**
+    /**
    * @public
    * @description toPercent
    * @param {Number} value
@@ -117,8 +118,9 @@ class UnitUtils {
    *
    * // OUTPUT '20%'
    */
-  toPercent = (value, total = 100, decimal = 2) => { // eslint-disable-line arrow-body-style
-    return `${Math.floor((value / total * 100) * (10 ** decimal)) / (10 ** decimal)}${this.UNIT.PERCENT}`; // eslint-disable-line no-mixed-operators
+    this.toPercent = (value, total = 100, decimal = 2) => { // eslint-disable-line arrow-body-style
+      return `${Math.floor((value / total * 100) * (10 ** decimal)) / (10 ** decimal)}${this.UNIT.PERCENT}`; // eslint-disable-line no-mixed-operators
+    };
   }
 }
 
